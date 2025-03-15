9f55baeeaf659ccb4dd8069d4e4db0fda33ce9e9
Souravs72 <souravsingh2609@gmail.com>
2025-03-15 12:24:28 +0530

    first commit

---
+++ employee-service/src/test/java/io/github/souravs72/employee_service/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.souravs72.employee_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
